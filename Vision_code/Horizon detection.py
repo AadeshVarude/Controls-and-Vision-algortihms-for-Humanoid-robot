0d37b779155056b30459b0a98fd45764dbf2717b
AadeshVarude <50541542+AadeshVarude@users.noreply.github.com>
2021-07-06 11:43:44 +0530

    Update Horizon detection.py

--- Vision_code/Horizon detection.py
+++ Vision_code/Horizon detection.py
@@ -11,15 +11,17 @@ cv2.createTrackbar("U – H", "Trackbars", 179, 179, nothing)
 cv2.createTrackbar("U – S", "Trackbars", 255, 255, nothing)
 cv2.createTrackbar("U – V", "Trackbars", 255, 255, nothing)
 
-# img =cv2.imread("bitbots-montreal-test01_003263.jpg")
+img =cv2.imread("bitbots-montreal-test01_003263.jpg")
 # img =cv2.imread("bitbots-montreal-test01_000000.jpg")
-img =cv2.imread("football_grnd.png")
-# img =cv2.imread("bitbots-montreal-test01_0032226.jpg")
-
-img=cv2.GaussianBlur(img,(5,5),3)
+# img =cv2.imread("football_grnd.png")
+# img =cv2.imread("test_lab.jpg")
+a=np.shape(img)
+x=a[0]
+y=a[1]
+img=cv2.GaussianBlur(img,(3,3),10)
 canny=np.zeros(np.shape(img))
 canny=cv2.Canny(img,100,150)
-cv2.imshow("Img1",img)
+# cv2.imshow("Img1",img)
 while True:
     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
     l_h = cv2.getTrackbarPos("L – H", "Trackbars")
@@ -28,29 +30,45 @@ while True:
     u_h = cv2.getTrackbarPos("U – H", "Trackbars")
     u_s = cv2.getTrackbarPos("U – S", "Trackbars")
     u_v = cv2.getTrackbarPos("U – V", "Trackbars")
-    # lower_value = np.array([31, 100, 0])
-    # upper_value = np.array([120, 255, 180])
+    # lower_value = np.array([121, 100, 31.4])
+    # upper_value = np.array([170, 100, 31.4])
     lower_value = np.array([l_h, l_s, l_v])
     upper_value = np.array([u_h, u_s, u_v])
 
     mask= cv2.inRange(hsv,lower_value,upper_value)
-    canny = cv2.Canny(mask, 100, 100)
-    cv2.imshow("canny",canny)
+    cv2.imshow("mask", mask)
 
     key=cv2.waitKey(1)
     if key==27:
         break
-
-
-
-lines = cv2.HoughLinesP(canny, 1, np.pi / 180,1)
+kernel = np.ones((5, 5), np.uint8)
+mask= cv2.dilate(mask,kernel,iterations=15)
+canny = cv2.Canny(mask, 100, 100)
+cv2.imshow("canny",canny)
+lines = cv2.HoughLines(canny, 0.7, np.pi/120, 120, min_theta=np.pi/36, max_theta=np.pi-np.pi/36)
 
 for line in lines:
-    x1, y1, x2, y2 = line[0]
-    cv2.line(img, (x1, y1), (x2, y2), (0, 255, 0), 1)
-cv2.imshow("Image Line",img)
+    rho,theta = line[0]
+    a = np.cos(theta)
+    b = np.sin(theta)
+    x0 = a*rho
+    y0 = b*rho
+    x1 = int(x0 + 10000*(-b))
+    y1 = int(y0 + 10000*(a))
+    x2 = int(x0 - 10000*(-b))
+    y2 = int(y0 - 10000*(a))
+    cv2.line(img,(x1,y1),(x2,y2),(0,255,0),1)
 
-result = cv2.bitwise_and(img, img, mask=mask)
-cv2.imshow("result",result)
+
+# Removing teh pixels that are above the horizon
+m=(y2-y1)/(x2-x1)
+c=y1-(m*x1)
+print(c)
+for i in range(x-1):
+    for j in range(y-1):
+        if 0 < (j*m+c)-i:
+            img[i][j]=0
+
+cv2.imshow("lined",img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
